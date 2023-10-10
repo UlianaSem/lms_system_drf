af93be12dcb77a1c57df8bb00c8fea5eb4ae9a0e
Uliana Semyonova <i@ulianasemyonova.ru>
2023-10-10 20:17:28 +0500

    Add djangorestframework-simplejwt

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.apps import UsersConfig
 from users.views import UserUpdateView, UserDetailView
@@ -8,4 +12,6 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
     path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
