82425db3533db8c7f298ad91c674a3514fe4fd29
Uliana Semyonova <i@ulianasemyonova.ru>
2023-10-10 20:17:06 +0500

    Add djangorestframework-simplejwt

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_filters',
+    'rest_framework_simplejwt',
 
     'users',
     'courses',
@@ -145,5 +146,8 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import RetrieveAPIView, UpdateAPIView, CreateAPIView, DestroyAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from courses.models import Course, Lesson, Payment
@@ -13,6 +14,7 @@ class CourseViewSet(ModelViewSet):
     serializers = {
         'retrieve': CourseDetailSerializer
     }
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         return self.serializers.get(self.action, self.default_serializer)
@@ -21,11 +23,13 @@ class CourseViewSet(ModelViewSet):
 class LessonCreateView(CreateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonUpdateView(UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonDeleteView(DestroyAPIView):
@@ -36,16 +40,19 @@ class LessonDeleteView(DestroyAPIView):
 class LessonDetailView(RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonListView(ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class PaymentListView(ListAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+    permission_classes = [IsAuthenticated]
 
     filter_backends = (DjangoFilterBackend, OrderingFilter)
     filterset_fields = ('course', 'lesson', 'method', )
